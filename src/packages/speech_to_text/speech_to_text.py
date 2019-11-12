258410f65f733e05b18ba67355213046cba3f28d
Nashua <nashua_luk_17@hotmail.com>
2019-11-11 23:49:47 -0500

    initial code for speech to text

---
+++ src/packages/speech_to_text/speech_to_text.py
@@ -0,0 +1,36 @@
+import io
+import os
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+class SpeechToText:
+    def__init__(self, filepath):
+
+    def analyze_text(self, filepath):
+        # Instantiates a client
+        client = speech.SpeechClient()
+
+        # The name of the audio file to transcribe
+        file_name = os.path.join(
+            os.path.dirname(__file__),
+            'resources',
+            'audio.raw')
+
+        # Loads the audio into memory
+        with io.open(file_name, 'rb') as audio_file:
+            content = audio_file.read()
+            audio = types.RecognitionAudio(content=content)
+
+        config = types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=16000,
+            language_code='en-US')
+
+        # Detects speech in the audio file
+        response = client.recognize(config, audio)
+
+        for result in response.results:
+            print('Transcript: {}'.format(result.alternatives[0].transcript))
+        
